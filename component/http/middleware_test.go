a9353ebe5d5f0c1f612678022e7128e8cf854dce
teivah <t.harsanyi@thebeat.co>
2020-07-27 11:14:40 +0200

    Strip query string

--- component/http/middleware_test.go
+++ component/http/middleware_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // A middleware generator that tags resp for assertions
@@ -117,4 +118,57 @@ func TestResponseWriter(t *testing.T) {
 	assert.Len(t, rw.Header(), 1, "Header count expected to be 1")
 	assert.True(t, rw.statusHeaderWritten, "expected to be true")
 	assert.Equal(t, "test", rc.Body.String(), "body expected to be test but was %s", rc.Body.String())
+}
+
+func TestStripQueryString(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := map[string]struct {
+		args         args
+		expectedPath string
+		expectedErr  error
+	}{
+		"query string 1": {
+			args: args{
+				path: "foo?bar=value1&baz=value2",
+			},
+			expectedPath: "foo",
+		},
+		"query string 2": {
+			args: args{
+				path: "/foo?bar=value1&baz=value2",
+			},
+			expectedPath: "/foo",
+		},
+		"query string 3": {
+			args: args{
+				path: "http://foo/bar?baz=value1",
+			},
+			expectedPath: "http://foo/bar",
+		},
+		"no query string": {
+			args: args{
+				path: "http://foo/bar",
+			},
+			expectedPath: "http://foo/bar",
+		},
+		"empty": {
+			args: args{
+				path: "",
+			},
+			expectedPath: "",
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			s, err := stripQueryString(tt.args.path)
+			if tt.expectedErr != nil {
+				assert.EqualError(t, err, tt.expectedErr.Error())
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, tt.expectedPath, s)
+			}
+		})
+	}
 }
